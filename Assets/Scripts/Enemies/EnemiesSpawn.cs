0e283f2d114489a7795d9aa0067402ec88540b32
JakubGrezl <jakub.grezl@outlook.cz>
2023-03-11 20:54:57 +0100

    XD

--- Assets/Scripts/Enemies/EnemiesSpawn.cs
+++ Assets/Scripts/Enemies/EnemiesSpawn.cs
@@ -1,99 +1,63 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemiesSpawn : MonoBehaviour
 {
-    List<GameObject> enemies = new List<GameObject>();
+    public GameObject enemyPrefab; // The enemy prefab to spawn
+    public int enemyCount = 10; // The number of enemies to spawn
+    public float minDistanceFromSurface = 1f; // The minimum distance from the sphere's surface at which to spawn enemies
+    public float maxDistanceFromSurface = 5f; // The maximum distance from the sphere's surface at which to spawn enemies
 
-    [SerializeField]
-    GameObject enemy;
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+    private List<GameObject> enemyPool = new List<GameObject>(); // The pool of enemy game objects
 
-    Mesh mesh;
-    MeshFilter meshFilter;
+    
 
-    private void Start()
+    void Start()
     {
-        SpawnEnemies(20);
-    }
-
-    void SpawnEnemies(int count)
-    {
+        
+        // Get a reference to the sphere's mesh filter component
         meshFilter = GetComponent<MeshFilter>();
         mesh = meshFilter.mesh;
 
-        for (int i = 0; i < count; i++)
+        // Create the pool of enemy game objects
+        for (int i = 0; i < enemyCount; i++)
         {
-            Vector3 point = GetRandomPointOnMesh(mesh);
+            GameObject enemy = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
+            enemy.SetActive(false); // Deactivate the enemy so it's not visible in the scene
+            enemyPool.Add(enemy);
+        }
 
-            var init = Instantiate(enemy, point, Quaternion.identity);
+        // Spawn the enemies
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Vector3 randomPoint = GetRandomPointOnMesh(mesh);
 
-            enemies.Add(init);
+            // Calculate a random distance from the sphere's surface
+            float distanceFromSurface = Random.Range(minDistanceFromSurface, maxDistanceFromSurface);
+
+            // Calculate the spawn position based on the random point and distance from the surface
+            Vector3 spawnPosition = randomPoint + distanceFromSurface * (randomPoint - transform.position).normalized;
+
+            // Activate an enemy from the pool and set its position and rotation
+            GameObject enemy = enemyPool[i];
+            enemy.transform.position = spawnPosition;
+            enemy.transform.rotation = Quaternion.identity;
+            enemy.SetActive(true);
         }
     }
 
-    
-        Vector3 GetRandomPointOnMesh(Mesh mesh)
-        {
-            //if you're repeatedly doing this on a single mesh, you'll likely want to cache cumulativeSizes and total
-            float[] sizes = GetTriSizes(mesh.triangles, mesh.vertices);
-            float[] cumulativeSizes = new float[sizes.Length];
-            float total = 0;
-
-            for (int i = 0; i < sizes.Length; i++)
-            {
-                total += sizes[i];
-                cumulativeSizes[i] = total;
-            }
-
-            //so everything above this point wants to be factored out
-
-            float randomsample = Random.value * total;
-
-            int triIndex = -1;
-
-            for (int i = 0; i < sizes.Length; i++)
-            {
-                if (randomsample <= cumulativeSizes[i])
-                {
-                    triIndex = i;
-                    break;
-                }
-            }
-
-            if (triIndex == -1) Debug.LogError("triIndex should never be -1");
-
-            Vector3 a = mesh.vertices[mesh.triangles[triIndex * 3]];
-            Vector3 b = mesh.vertices[mesh.triangles[triIndex * 3 + 1]];
-            Vector3 c = mesh.vertices[mesh.triangles[triIndex * 3 + 2]];
-
-            //generate random barycentric coordinates
-
-            float r = Random.value;
-            float s = Random.value;
-
-            if (r + s >= 1)
-            {
-                r = 1 - r;
-                s = 1 - s;
-            }
-            //and then turn them back to a Vector3
-            Vector3 pointOnMesh = a + r * (b - a) + s * (c - a);
-            return pointOnMesh;
-
-        }
-
-    float[] GetTriSizes(int[] tris, Vector3[] verts)
+    Vector3 GetRandomPointOnMesh(Mesh mesh)
     {
-        int triCount = tris.Length / 3;
-        float[] sizes = new float[triCount];
-        for (int i = 0; i < triCount; i++)
-        {
-            sizes[i] = .5f * Vector3.Cross(verts[tris[i * 3 + 1]] - verts[tris[i * 3]], verts[tris[i * 3 + 2]] - verts[tris[i * 3]]).magnitude;
-        }
-        return sizes;
+        // Get a random vertex from the mesh
+        Vector3[] vertices = mesh.vertices;
+        int randomIndex = Random.Range(0, vertices.Length);
+        Vector3 randomVertex = vertices[randomIndex];
 
+        // Transform the vertex position from local space to world space
+        Vector3 randomPoint = transform.TransformPoint(randomVertex);
+
+        return randomPoint;
     }
-
-
 }
