cbe4bf66b0a32c20027df6a356a7c89215441b57
creeperplayer20 <jdolezalek6@gmail.com>
2023-03-11 16:34:02 +0100

    Recovery

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -7,11 +7,7 @@ public class Rifle : Weapon
     protected override void ProcessShootingInput()
     {
         if (Input.GetButton("Fire1"))
-        {
             if (currentAmmoCount > 0 && currentShootCooldown <= 0 && !IsReloading)
-            {
                 Shoot();
-            }
-        }
     }
 }
